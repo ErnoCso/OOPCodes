514a8cac081f78c320a87859e6126a9e05c6236a
c3682905 <E.Csorvasi2742@student.leedsbeckett.ac.uk>
2024-01-30 10:39:35 +0000

    \Tasks of the first week.

---
+++ W1_T3/src/PersonalDetails.java
@@ -0,0 +1,67 @@
+
+//	3) Create a second project called “PersonalDetails” and write a program that outputs
+//	   some personal details to the screen, e.g., Name, Address, Age, Phone number etc.
+
+import java.util.Scanner;
+
+public class PersonalDetails {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		String name;
+		String address;
+		int age;
+		String phone;
+		String email;
+		String addrHouse;
+		String addrCity;
+		String addrPost;
+		
+        Scanner scan = new Scanner(System.in);
+        System.out.println("==================================");
+        System.out.println("=<       Personal Details       >=");
+        System.out.println("==================================");
+        System.out.print("\nEnter your full name: " );
+        name = scan.nextLine();
+        System.out.println("Enter your address " );
+        System.out.print("House Number, Street Name: ");
+        addrHouse = scan.nextLine();
+        System.out.print("City and County name: ");
+        addrCity = scan.nextLine();
+        System.out.print("PostCode: ");
+        addrPost = scan.nextLine();
+        System.out.print("Enter your age in integers: " );
+        age = scan.nextInt();
+        System.out.print("Enter your phone number: " );
+        phone = scan.next();
+        System.out.print("Enter your email address: " );
+        email = scan.next();
+
+        scan.close();
+       
+ 
+        System.out.println("\nYour details: " + "\n============="+ "\n\nName: \t\t" + name + 
+        		"\n\nAddress: \t" + addrHouse + "\n\t\t" + addrCity + "\n\t\t" + addrPost + 
+        		"\n\nAge: \t\t" + age + " years old" + "\n\nPhone no: \t" + phone + 
+        		"\n\nEmail addr: \t" + email);
+	}
+
+}
+
+//	Output:
+//	
+//	Your details: 
+// =============
+//
+// Name: 		James Bond
+//
+// Address: 	01. Magic Java Road
+//				Leeds
+//				LS1 1AA
+//
+// Age: 		24 years old
+//
+// Phone no: 	07462385129
+//
+// Email addr: 	magicalworld@magic.ac.uk
---
+++ W1_T4/src/DiamondShape.java
@@ -0,0 +1,69 @@
+// Write and application that prints the following diamond shape. 
+// Don’t print any unneeded characters.
+//
+//               *
+//	            ***
+//	           *****
+//	          *******
+//             *****
+//              ***
+//               *
+
+
+public class DiamondShape {
+	
+	public static void main(String[] args) {
+	
+
+        System.out.println("===================================");
+        System.out.println("=<            DIAMOND            >=");
+        System.out.println("===================================\n");
+
+
+        int n = 7;
+        int s = 6;
+        
+        for (int x = 1; x <= n; x+=2) {
+        	for (int y = -11; y < s; y++) {
+        		System.out.print(" ");
+        	} 
+        	for (int y = 0; y < x; y++) { 
+        		System.out.print("*");
+        		
+        	}
+        	System.out.println();
+        	s -= 1;
+        
+        }
+        s = 1;
+        
+        for (int x = n-2; x >= 1; x -= 2) {
+        	for (int y = -14; y < s; y++) {
+        		System.out.print(" ");
+        	} 
+        	for (int y = 0; y < x; y += 1) { 
+        		System.out.print("*");
+        		
+        	}
+	        System.out.println();
+	        s +=1;
+        }
+	}
+
+}
+
+
+
+// Output:
+//
+// ===================================
+// =<            DIAMOND            >=
+// ===================================
+//
+//                  * 
+//                * * * 
+//              * * * * * 
+//            * * * * * * * 
+//              * * * * * 
+//                * * * 
+//                  * 
