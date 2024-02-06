04d1031c09fd5fa83fac820121265e7b52d33cb9
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2024-02-06 23:30:25 +0000

    Add files via upload

---
+++ Week_1/HelloWorld/src/W1_T1.java
@@ -0,0 +1,18 @@
+
+// 1) Create a new project called “HelloWorld” and write a simple Java program 
+// that displays the message “Hello World” to the console.
+
+
+public class W1_T1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
+
+// Output:
+//
+// Hello World!
